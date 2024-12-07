9745fa862cefe71d77fdc275d1b6fb490b40c250
Kim Cortes-Martinez <kcortesmartinez@college.harvard.edu>
2024-12-07 18:45:12 +0000

    organizing + bugs

--- coffeecraze/backend-files/server.js
+++ coffeecraze/backend-files/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 // Connects to the database
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then() => console.log("Connected to MongoDB")
+    .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.error("Database connection error:", err));
 
 // Routes
